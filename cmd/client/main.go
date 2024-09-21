8e5d6ec7462b07ddc6fa8eb2f13b7a7584cff4df
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-09-21 21:31:03 +0200

    server repl

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"os"
+	"os/signal"
+)
+
+const rabbitMQURL = "amqp://guest:guest@localhost:5672/"
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	connection, err := amqp.Dial(rabbitMQURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer connection.Close()
+
+	log.Println("Rabbit MQ connection successful")
+
+	_, _, err = pubsub.DeclareAndBind(
+		connection,
+		routing.ExchangePerilDirect,
+		fmt.Sprintf("%s.%s", routing.PauseKey, username),
+		routing.PauseKey,
+		pubsub.Transient,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Wait for interrupt signal
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	log.Println("Shutting Down")
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,90 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	"log"
+)
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const rabbitMQURL = "amqp://guest:guest@localhost:5672/"
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connection, err := amqp.Dial(rabbitMQURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer connection.Close()
+
+	log.Println("Rabbit MQ connection successful")
+
+	mqChan, err := connection.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = pubsub.PublishJSON(
+		mqChan,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{IsPaused: true},
+	); err != nil {
+		log.Fatal(err)
+	}
+
+	gamelogic.PrintServerHelp()
+
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		if words[0] == "pause" {
+			fmt.Println("sending pause message...")
+			if err = pubsub.PublishJSON(
+				mqChan,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{IsPaused: true},
+			); err != nil {
+				log.Fatal(err)
+			}
+			continue
+		}
+
+		if words[0] == "resume" {
+			fmt.Println("sending resume message...")
+			if err = pubsub.PublishJSON(
+				mqChan,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{IsPaused: false},
+			); err != nil {
+				log.Fatal(err)
+			}
+			continue
+		}
+
+		if words[0] == "quit" {
+			fmt.Println("Stopping...")
+			break
+		}
+
+		fmt.Println("I don't understand. Either use 'pause', 'resume' or 'quit'")
+	}
+
+	//// Wait for interrupt signal
+	//signalChan := make(chan os.Signal, 1)
+	//signal.Notify(signalChan, os.Interrupt)
+	//<-signalChan
+
+	log.Println("Shutting Down")
 }
